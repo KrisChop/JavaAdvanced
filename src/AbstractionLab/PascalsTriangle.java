5f117fee7fd65f30c7f592426f0ea9fd46c8e805
Kristiyan <32292621+KrisChop@users.noreply.github.com>
2017-09-27 11:55:07 +0300

    PascalsTriangle 100/100

--- src/AbstractionLab/PascalsTriangle.java
+++ src/AbstractionLab/PascalsTriangle.java
@@ -1,5 +1,6 @@
 package AbstractionLab;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class PascalsTriangle {
@@ -7,25 +8,25 @@ public class PascalsTriangle {
         Scanner scanner = new Scanner(System.in);
         int size = Integer.parseInt(scanner.nextLine());
 
-        long[][] pascal = new long[size][];
+        BigInteger[][] pascal = new BigInteger[size][];
 
         for (int i = 0; i < pascal.length; i++) {
-            pascal[i] = new long[i + 1];
+            pascal[i] = new BigInteger[i + 1];
         }
 
-        pascal[0][0] = 1;
+        pascal[0][0] = BigInteger.ONE;
 
         for (int row = 1; row < pascal.length; row++) {
-            pascal[row][0] = 1;
-            pascal[row][pascal[row].length - 1] = 1;
+            pascal[row][0] = BigInteger.ONE;
+            pascal[row][pascal[row].length - 1] = BigInteger.ONE;
 
             for (int col = 1; col < pascal[row].length - 1; col++) {
-                pascal[row][col] = pascal[row - 1][col - 1] + pascal[row - 1][col];
+                pascal[row][col] = pascal[row - 1][col - 1].add(pascal[row - 1][col]);
             }
         }
 
-        for (long[] longs : pascal) {
-            for (long aLong : longs) {
+        for (BigInteger[] longs : pascal) {
+            for (BigInteger aLong : longs) {
                 System.out.print(aLong + " ");
             }
             System.out.println();
